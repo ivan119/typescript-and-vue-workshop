2995703652a344f3392430a6a5aeab331bc31830
ivan119 <ivankelava91@gmail.com>
2023-08-24 22:38:17 +0200

    feat: global transition animation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,23 @@ import Navbar from '@/components/Navbar.vue'
 
 <template>
   <navbar />
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+  </router-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition:  opacity 1s, transform 1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+
+</style>
--- src/views/DishPage.vue
+++ src/views/DishPage.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <main class="main">
+    <h1 class="h1">Dish Page</h1>
+  </main>
+</template>
+
+<style scoped>
+
+.main {
+  height: calc(100vh - 88px);
+  @apply w-full flex flex-col justify-center items-center
+}
+.h1 {
+  @apply text-green-400 from-green-500 to-blue-300 text-4xl
+}
+</style>
